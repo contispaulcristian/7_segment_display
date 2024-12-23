4b595323433874b4c5f5bcb2e0fd161f6c3abd00
paul.contis <contispaulcristian@gmail.com>
2024-12-23 19:52:35 +0200

    empty project

---
+++ Programming/7SegmentDisplay/7SegmentDisplay/main.c
@@ -0,0 +1,18 @@
+/*
+ * 7SegmentDisplay.c
+ *
+ * Created: 12/23/2024 7:51:45 PM
+ * Author : conti
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
